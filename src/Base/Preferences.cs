96a2230f8f99d6935129994a62e9554f7cc7f2a4
AluminiumTech <contact@aluminiumte.ch>
2021-05-18 01:47:25 +0100

    add try/catch to preferences and fix bad naming of Modify method

--- src/Base/Preferences.cs
+++ src/Base/Preferences.cs
@@ -1,7 +1,7 @@
 /*
 MIT License
 
-Copyright (c) 2020 AluminiumTech
+Copyright (c) 2021 AluminiumTech
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -22,6 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
+using System;
 using System.Collections.Generic;
 
 namespace AluminiumTech.DevKit.SettingsKit.Base
@@ -47,42 +48,67 @@ namespace AluminiumTech.DevKit.SettingsKit.Base
 
         public void Modify(KeyValuePair<TKey, TValue> oldPreference, KeyValuePair<TKey, TValue> newPreference)
         { 
-            Insert(GetPosition(oldPreference), newPreference);
-            Remove(oldPreference);
+            try
+            {
+                Insert(GetPosition(oldPreference), newPreference);
+                Remove(oldPreference);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                throw new Exception(ex.ToString());
+            }
         }
 
+        /// <summary>
+        /// Gets the position of a preference within this Preferences object.
+        /// <param name="preference"></param>
+        /// <returns></returns>
         public int GetPosition(KeyValuePair<TKey, TValue> preference)
         {
-            for(int i = 0; i < this.Count; i++)
+            try
             {
-                if (this[i].Equals(preference))
+                for (int i = 0; i < this.Count; i++)
                 {
-                    return i;
+                    if (this[i].Equals(preference))
+                    {
+                        return i;
+                    }
                 }
-            }
 
-            return 0;
+                return 0;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                throw new Exception(ex.ToString());
+            }
         }
 
         /// <summary>
         /// Get the position of the Key within a list of HashMapWrappers
-        /// Note:
-        ///     First int in Tuple is the selected HashMapWrapper in the list.
-        ///     Second int in Tuple is the position in the selected HashMapWrapper
         /// </summary>
         /// <param name="key"></param>
         /// <returns></returns>
         public int GetPosition(TKey key)
-        {
-            for (int index = 0; index < this.Count; index++)
+        {           
+            try
             {
-                if (this[index].Equals(key))
+                for (int index = 0; index < this.Count; index++)
                 {
-                    return index;
+                    if (this[index].Equals(key))
+                    {
+                        return index;
+                    }
                 }
-            }
 
-            return 0;
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                throw new Exception(ex.ToString());
+            }
         }
     }
 }
