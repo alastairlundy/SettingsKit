1c106a698fef9b9d2a2673cea6e5e91a462de41d
AluminiumTech <14107316+AluminiumTech@users.noreply.github.com>
2021-10-19 14:37:16 +0100

    make sure our new code works

---
+++ SettingsKitTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+using AluminiumTech.DevKit.SettingsKit;
+
+namespace SettingsKitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string path = Environment.CurrentDirectory + Path.DirectorySeparatorChar + "Testing.json";
+
+            Preferences<string, string> preferences = new Preferences<string, string>(path);
+
+            preferences.LoadPreferences(path);
+            
+            //preferences.AddPreference("aluminium_state", "awake", "awake");
+            
+            Console.WriteLine(preferences.GetPreference("aluminium_emotion").ToString());
+            Console.WriteLine(preferences.GetPreference("aluminium_state").ToString());
+            
+        //    preferences.UpdatePreference("aluminium_emotion", "sad", "sad");
+            
+           // Console.WriteLine(preferences.GetPreference("aluminium_emotion").ToString());
+        }
+    }
+}
