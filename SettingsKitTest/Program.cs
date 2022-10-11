968a7b45fb307b7aa9eaf5f3c1ae6d877c6297d1
AluminiumTech <14107316+AluminiumTech@users.noreply.github.com>
2022-10-11 18:49:41 +0100

    update test

--- SettingsKitTest/Program.cs
+++ SettingsKitTest/Program.cs
@@ -14,12 +14,16 @@ namespace SettingsKitTest
 
             string path = Environment.CurrentDirectory + Path.DirectorySeparatorChar + "Testing.json";
 
-            DataManager<string, string> preferences = new DataManager<string, string>(path);
+            SettingsManager<string, string> preferences = new SettingsManager<string, string>(path);
             
-            preferences.LoadData(path);
+            //preferences.LoadData(path);
 
-         //   preferences.AddData("aluminium_state", "awake", "super");
-         //   preferences.AddData("aluminium_emotion", "happy", "dope");
+            preferences.AddData("aluminium_state", "awake");
+            preferences.AddData("aluminium_emotion", "happy");
+            preferences.AddData("pascal_replying_to_me", "no");
+            preferences.AddData("time_to_eat", "not_now");
+
+            preferences.WriteJsonFile(path);
             
             Console.WriteLine(preferences.GetData("aluminium_emotion").ToString());
            Console.WriteLine(preferences.GetData("aluminium_state").ToString());
