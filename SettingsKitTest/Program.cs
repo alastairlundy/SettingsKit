02a9d7174b12b728cd8c057cc31f749c0fdb3439
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2023-07-10 19:38:55 +0100

    update test program to reflect new code

--- SettingsKitTest/Program.cs
+++ SettingsKitTest/Program.cs
@@ -1,8 +1,31 @@
-﻿using System;
+﻿/*
+MIT License
+
+Copyright (c) 2021-2023 Alastair Lundy
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.IO;
-
-using AluminiumTech.DevKit.SettingsKit;
+using AlastairLundy.SettingsKit;
 
 namespace SettingsKitTest
 {
@@ -14,20 +37,9 @@ namespace SettingsKitTest
 
             string path = Environment.CurrentDirectory + Path.DirectorySeparatorChar + "Testing.json";
 
-            SettingsManager<string, string> preferences = new SettingsManager<string, string>(path);
+            JsonSettingsProvider<string, string> preferences = new JsonSettingsProvider<string, string>();
+   
             
-            //preferences.LoadData(path);
-
-            preferences.Set("aluminium_state", "awake");
-            preferences.Set("aluminium_emotion", "happy");
-            preferences.Set("pascal_replying_to_me", "no");
-            preferences.Set("time_to_eat", "not_now");
-
-            preferences.WriteJsonFile(path);
-            
-            Console.WriteLine(preferences.Get("aluminium_emotion").ToString());
-           Console.WriteLine(preferences.Get("aluminium_state").ToString());
-           Console.WriteLine(preferences.Get("pascal_replying_to_me").ToString()); 
            
         //    preferences.UpdatePreference("aluminium_emotion", "sad", "sad");
         }
