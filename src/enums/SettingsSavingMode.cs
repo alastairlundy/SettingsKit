40c085b1b0478ae1a55acf32b286b3377c105e56
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2023-07-11 18:19:36 +0100

    fix copyright notices

--- src/enums/SettingsSavingMode.cs
+++ src/enums/SettingsSavingMode.cs
@@ -1,7 +1,7 @@
 ﻿/*
 MIT License
 
-Copyright (c) 2021-2022 AluminiumTech
+Copyright (c) 2021-2023 Alastair Lundy
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
