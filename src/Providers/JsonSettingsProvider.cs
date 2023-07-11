db07f2a1bd265415bf8cef2cf6f51e6cb95463d0
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2023-07-11 18:11:37 +0100

    update Provider namespace

--- src/Providers/JsonSettingsProvider.cs
+++ src/Providers/JsonSettingsProvider.cs
@@ -29,7 +29,7 @@ using System.Text.Json;
 
 using AlastairLundy.SettingsKit.Interfaces;
 
-namespace AlastairLundy.SettingsKit
+namespace AlastairLundy.SettingsKit.Providers
 {
     /// <summary>
     /// 
