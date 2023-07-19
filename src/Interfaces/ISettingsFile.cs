03a64d1124fab93da9add04b0728e8e9f490563e
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2023-07-19 18:17:24 +0100

    remove the KeyValuePairs from the ISettingsFile

--- src/Interfaces/ISettingsFile.cs
+++ src/Interfaces/ISettingsFile.cs
@@ -28,8 +28,6 @@ namespace AlastairLundy.SettingsKit;
 
 public interface ISettingsFile<TKey, TValue>
 {
-    internal List<KeyValuePair<TKey, TValue>> KeyValuePairs { get; set; }
-    
     public ISettingsProvider<TKey, TValue> SettingsProvider { get; }
 
     public void Add(KeyValuePair<TKey, TValue> pair);
